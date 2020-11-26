2de37abdf920f5ccfec01c87931110e43fa901b4
Fabio <fabio.mezzina.09@gmail.com>
2020-11-26 12:25:00 +0100

    add poster img

--- js/main.js
+++ js/main.js
@@ -7,13 +7,19 @@ const app = new Vue({
     searchLanguage: 'it-IT',
   }, // <- End Data
   methods: {
+    /**
+     * Start search and clear input search bar
+     */
     search() {
       this.getMovies();
       this.getSeries();
       this.searchInput = '';
     },
+    /**
+     * Search for movies and populate movies array
+     */
     getMovies() {
-      if (this.searchInput) {
+      if (this.searchInput.trim()) {
         axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
                   api_key: 'c7984b175d921dd492707f69a01be0da',
@@ -29,8 +35,11 @@ const app = new Vue({
             });
       }
     },
+    /**
+     * Search for tv series and populate series array
+     */
     getSeries() {
-      if (this.searchInput) {
+      if (this.searchInput.trim()) {
         axios.get('https://api.themoviedb.org/3/search/tv', {
                 params: {
                   api_key: 'c7984b175d921dd492707f69a01be0da',
@@ -46,8 +55,19 @@ const app = new Vue({
             });
       }
     },
+    /**
+     * Convert and return film rating, from 1-10 to 1-5
+     * @param {number} numRating 
+     */
     setStarRating(numRating) {
       return Math.ceil(numRating / 2);
     },
+    /**
+     * Generate and return the poster img url
+     * @param {string} path 
+     */
+    setPosterPath(path) {
+      return `https://image.tmdb.org/t/p/w342/${path}`;
+    }
   } // <- End Methods
 });
